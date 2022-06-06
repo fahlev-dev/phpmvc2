7d6adc6efcc4b4d8d6a193ab2d92ebc44eedb559
Reza Fahlevi <rfahlevi501@gmail.com>
2022-06-06 22:50:48 +0700

    menambahkan file index.php di folder views/home, menambahkan file index.php dan page.php di folder views/about, menambahkan templates header.php dan footer.php di folder views/templates

---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>Ini adalah halaman about/index</h1>
+    <p>halo nama saya adalah <?= $data['nama']; ?>, umur saya adalah <?= $data['umur']; ?> tahun, pekerjaan saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
---
+++ app/views/about/page.php
@@ -0,0 +1,2 @@
+
+    <h1>Ini adalah halaman about/page</h1>

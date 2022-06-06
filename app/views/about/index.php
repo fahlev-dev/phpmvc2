90d8afe133b65775d392667589d58e15b91e5fee
Reza Fahlevi <rfahlevi501@gmail.com>
2022-06-06 23:41:12 +0700

    memodifikasi file index.php di folder views/about, views/home, dan folder views/templates

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,4 @@
 
-    <h1>Ini adalah halaman about/index</h1>
+    <h1 class="mt-4">Ini adalah halaman about/index</h1>
+    <img src="<?= BASEURL; ?>/img/sandhika.jpeg" alt="" class="rounded-circle shadow">
     <p>halo nama saya adalah <?= $data['nama']; ?>, umur saya adalah <?= $data['umur']; ?> tahun, pekerjaan saya adalah seorang <?= $data['pekerjaan']; ?>.</p>

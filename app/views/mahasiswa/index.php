59b00b037a9f1c9ad4da7d234a4662aedd293052
Reza Fahlevi <rfahlevi501@gmail.com>
2022-06-10 16:33:33 +0700

    menambahkan tombol ubah dan konfigurasinya

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -8,7 +8,7 @@
 <div class="row">
     <div class="col-lg-6">
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary mt-3" data-toggle="modal" data-target="#formModal">
+        <button type="button" class="btn btn-primary mt-3 tombolTambahData" data-toggle="modal" data-target="#formModal">
             Tambah Data Mahasiswa
         </button>
         <br><br>
@@ -17,6 +17,7 @@
             <?php foreach( $data['mhs'] as $mhs) : ?>
                 <li class="list-group-item"><?= $mhs['nama']; ?>
                   <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onclick=" return confirm('Yakin!')">Hapus</a>
+                  <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">Edit</a>
                   <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">Detail</a>
                 </li>
             <?php endforeach; ?>
@@ -26,11 +27,11 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -38,6 +39,7 @@
       <div class="modal-body">
 
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+          <input type="hidden" name="id" id="id">
         <div class="form-group">
             <label for="nama">Nama</label>
             <input type="text" class="form-control" id="nama" name="nama">
